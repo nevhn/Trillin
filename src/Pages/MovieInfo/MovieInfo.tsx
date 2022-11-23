1a041fd1389fffefb006462ebbd90f2aca02e9b0
exhaustcs <exhaustcs@gmail.com>
2022-11-22 22:58:29 -0500

    Finshed implementing the api on the movie info page

--- src/Pages/MovieInfo/MovieInfo.tsx
+++ src/Pages/MovieInfo/MovieInfo.tsx
@@ -17,26 +17,62 @@ export const MovieInfo = () => {
   const [movie, setMovie] = useState<any>({});
   const [trailerUrl, setTrailerUrl] = useState("");
   const [parentalRating, setParentalRating] = useState("");
+  const [cast, setCast] = useState<string[]>([]);
+  const [crew, setCrew] = useState("");
+  const [genre, setGenre] = useState<string[]>([]);
 
-  /**Get query params : movieID */
+  /**Get query params : movieID to pass to the api call*/
   let { movieId } = useParams();
+
   console.log(movieId);
 
-  /**Pass query parma to api */
   const fetchMovieInfo = async () => {
+    /** Fetch details about the movie, official trailer, cast */
     const response = await axios(
-      `https://api.themoviedb.org/3/movie/${movieId}?api_key=13f9b567969342bbfb2322ca39624376&language=en-US&append_to_response=release_dates`
+      `https://api.themoviedb.org/3/movie/${movieId}?api_key=13f9b567969342bbfb2322ca39624376&language=en-US&append_to_response=release_dates,credits,videos`
     );
-    const movieData = response.data;
+
+    const movieInfo = response.data;
     const releaseDates = response.data["release_dates"].results;
+    const credits = response.data["credits"];
+
+    /**Find parental rating */
     const usReleaseDate = releaseDates.find(
       (date: any) => date.iso_3166_1 === "US"
     );
+
     const certification = usReleaseDate.release_dates[0].certification;
 
-    setMovie(movieData);
+    /**Genre */
+
+    const listOfGenres: string[] = [];
+
+    movieInfo.genres.find((obj: any) => {
+      listOfGenres.push(obj.name);
+    });
+
+    const castArr = credits.cast;
+    const crewArr = credits.crew;
+
+    /**Find director and actors */
+    const director = crewArr.find((c: any) => c.job === "Director");
+
+    const actors: string[] = [];
+
+    castArr.forEach((obj: any) => {
+      actors.push(obj.name);
+    });
+
+    // console.log("filtered:", director);
+
+    setMovie(movieInfo);
     setParentalRating(certification);
+    setCrew(director["name"]);
+    setCast(actors);
+    setGenre(listOfGenres);
+
     console.log(response.data);
+    console.log(actors);
   };
 
   const fetchMovieTrailer = async () => {
@@ -106,23 +142,31 @@ export const MovieInfo = () => {
 
           {/* Directors & Writers */}
           <Text fontWeight={"bold"} as="span">
-            Creator{" "}
+            Director{" "}
           </Text>
-          <Text as="span">Hideaki Anno</Text>
+          <Text as="span">{crew}</Text>
           <br />
 
+          {/* Cast*/}
+          <Text fontWeight={"bold"} as="span">
+            Cast{" "}
+          </Text>
+          <Text overflowWrap={"anywhere"} as="span">
+            {cast}
+          </Text>
+          <br />
           {/* Genre*/}
 
           <Text fontWeight={"bold"} as="span">
             Genre{" "}
           </Text>
-          <Text as="span">M</Text>
+          <Text as="span">{genre}</Text>
           <br />
           {/* Run Time*/}
           <Text fontWeight={"bold"} as="span">
             Run Time{" "}
           </Text>
-          <Text as="span">2 hours</Text>
+          <Text as="span">{movie.runtime}</Text>
           {"\n"}
         </Box>
       </Flex>
